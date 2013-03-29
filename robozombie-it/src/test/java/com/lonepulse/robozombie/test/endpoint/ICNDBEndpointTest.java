3e59303bd35e37d86d47dccb549c678fc7b35dda
sahan <lahiru@lonepulse.com>
2013-03-29 23:53:21 +0530

    Update test cases to perform network communication off the main thread

--- robozombie-it/src/test/java/com/lonepulse/robozombie/test/endpoint/ICNDBEndpointTest.java
+++ robozombie-it/src/test/java/com/lonepulse/robozombie/test/endpoint/ICNDBEndpointTest.java
@@ -24,9 +24,18 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.apache.http.HttpResponse;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.lonepulse.robozombie.core.annotation.Asynchronous;
 import com.lonepulse.robozombie.core.annotation.Endpoint;
@@ -43,6 +52,8 @@ import com.lonepulse.robozombie.test.model.ICNDBResponse;
 import com.lonepulse.robozombie.test.model.ICNDBResponseArray;
 import com.lonepulse.robozombie.test.model.NorrisJoke;
 import com.lonepulse.robozombie.test.service.ICNDBService;
+import com.xtremelabs.robolectric.Robolectric;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
 
 /**
  * <p>Performs <b>Unit Testing</b> on the proxy of {@link ICNDBEndpoint}.
@@ -53,6 +64,7 @@ import com.lonepulse.robozombie.test.service.ICNDBService;
  * <br><br>
  * @author <a href="mailto:lahiru@lonepulse.com">Lahiru Sahan Jayasinghe</a>
  */
+@RunWith(RobolectricTestRunner.class)
 public class ICNDBEndpointTest {
 
 	
@@ -61,10 +73,16 @@ public class ICNDBEndpointTest {
 	 */
 	private static ICNDBEndpoint icndbEndpoint;
 	
+	/**
+	 * <p>A single thread executor which performs 
+	 * RoboZombie network calls off-of the main thread.
+	 */
+	private static ExecutorService executorService;
+	
 	
 	/**
 	 * <p>Sets up the test case by performing endpoint injection on 
-	 * {@link #icndbEndpoint} and {@link #icndbServiceInstantiated}.
+	 * {@link #icndbEndpoint}.
 	 * 
 	 * @throws java.lang.Exception
 	 * 			if the endpoint injection failed
@@ -72,33 +90,60 @@ public class ICNDBEndpointTest {
 	@BeforeClass
 	public static void setUp() throws Exception {
 		
+		executorService = Executors.newSingleThreadExecutor();
+		
 		icndbEndpoint = Zombie.infect(ICNDBService.class).getConstructedICNDBEndpoint();
 	}
 
 	/**
 	 * <p>Test method for {@link Endpoint#path()} and {@link Request#path()} 
 	 * with {@link JsonResponseParser}.
+	 * 
+	 * @throws Exception
+	 * 			if test terminated with an error
 	 */
 	@Test
-	public final void testPath() {
+	public final void testPath() throws Exception {
 		
-		ICNDBResponse icndbResponse = icndbEndpoint.random();
+		Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
 		
+		Future<ICNDBResponse> future = executorService.submit(new Callable<ICNDBResponse>() {
+
+			public ICNDBResponse call() throws Exception {
+
+				return icndbEndpoint.random();
+			}
+		});
 		
+		ICNDBResponse icndbResponse = future.get();
+			
 		assertNotNull(icndbResponse);
 		assertNotNull(icndbResponse.getValue());
 	}
 	
 	/**
 	 * <p>Test method for {@link Param} with {@link RequestMethod#HTTP_GET}.
+	 * 
+	 * @throws Exception
+	 * 			if test terminated with an error
 	 */
 	@Test
-	public final void testGETParams() {
+	public final void testGETParams() throws Exception {
 		
-		String firstName = "John";
-		String lastName = "Doe";
+		Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
 		
-		NorrisJoke norrisJoke = icndbEndpoint.random(firstName, lastName).getValue();
+		final String firstName = "John";
+		final String lastName = "Doe";
+		
+		Future<NorrisJoke> future = executorService.submit(new Callable<NorrisJoke>() {
+
+			public NorrisJoke call() throws Exception {
+
+				return icndbEndpoint.random(firstName, lastName).getValue();
+			}
+		});
+		
+		NorrisJoke norrisJoke = future.get();
 
 		assertTrue(norrisJoke.getJoke().contains(firstName));
 		assertTrue(norrisJoke.getJoke().contains(lastName));
@@ -106,11 +151,24 @@ public class ICNDBEndpointTest {
 	
 	/**
 	 * <p>Test method for {@link Request.Param}.
+	 * 
+	 * @throws Exception
+	 * 			if test terminated with an error
 	 */
 	@Test
-	public final void testConstantParams() {
+	public final void testConstantParams() throws Exception  {
 		
-		NorrisJoke norrisJoke = icndbEndpoint.randomJohnDoeJoke().getValue();
+		Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
+		
+		Future<NorrisJoke> future = executorService.submit(new Callable<NorrisJoke>() {
+
+			public NorrisJoke call() throws Exception {
+
+				return icndbEndpoint.randomJohnDoeJoke().getValue();
+			}
+		});
+		
+		NorrisJoke norrisJoke = future.get();
 		
 		assertTrue(norrisJoke.getJoke().contains("John"));
 		assertTrue(norrisJoke.getJoke().contains("Doe"));
@@ -118,11 +176,24 @@ public class ICNDBEndpointTest {
 	
 	/**
 	 * <p>Test method for {@link Rest} and {@link PathParam}.
+	 * 
+	 * @throws Exception
+	 * 			if test terminated with an error
 	 */
 	@Test
-	public final void testPathParam() {
+	public final void testPathParam() throws Exception {
 		
-		ICNDBResponseArray icndbResponse = icndbEndpoint.random("4");
+		Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
+		
+		Future<ICNDBResponseArray> future = executorService.submit(new Callable<ICNDBResponseArray>() {
+
+			public ICNDBResponseArray call() throws Exception {
+
+				return icndbEndpoint.random("4");
+			}
+		});
+		
+		ICNDBResponseArray icndbResponse = future.get();
 		
 		assertNotNull(icndbResponse);
 		assertTrue(icndbResponse.getValue().size() == 4);
@@ -130,37 +201,79 @@ public class ICNDBEndpointTest {
 	
 	/**
 	 * <p>Test method for {@link Asynchronous} and {@link AsyncHandler}.
+	 * 
+	 * @throws Exception
+	 * 			if test terminated with an error
 	 */
 	@Test
-	public final void testAsyncRequest() {
+	public final void testAsyncRequest() throws Exception {
+
+		Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
+		
+		final BlockingQueue<ICNDBResponseArray> queueParsedResponse = new ArrayBlockingQueue<ICNDBResponseArray>(1);
+		final BlockingQueue<HttpResponse> queueResponse = new ArrayBlockingQueue<HttpResponse>(1);
 		
 		ICNDBResponseArray synchronousResult = icndbEndpoint.randomAsync(new AsyncHandler<ICNDBResponseArray>() {
 
 			@Override
 			public void onSuccess(HttpResponse httpResponse, ICNDBResponseArray icndbResponseArray) {
 
-				assertNotNull(httpResponse);
-				assertNotNull(icndbResponseArray);
-				assertNotNull(icndbResponseArray.getValue());
-				assertTrue(icndbResponseArray.getValue().size() == 10);
+				queueResponse.offer(httpResponse);
+				queueParsedResponse.offer(icndbResponseArray);
 			}
 		});
 		
 		assertNull(synchronousResult);
+		
+		HttpResponse httpResponse = queueResponse.take();
+		assertNotNull(httpResponse);
+		
+		ICNDBResponseArray icndbResponseArray = queueParsedResponse.take();
+		assertNotNull(icndbResponseArray);
+		assertNotNull(icndbResponseArray.getValue());
+		assertTrue(icndbResponseArray.getValue().size() == 10);
 	}
 	
 	/**
 	 * <p>Test method for {@link Header}.
+	 * 
+	 * @throws Exception
+	 * 			if test terminated with an error
 	 */
 	@Test
-	public final void testResponseHeader() {
+	public final void testResponseHeader() throws Exception {
 		
-		StringBuilder header = new StringBuilder();
+		Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
 		
-		assertNotNull(icndbEndpoint.random(header));
+		final StringBuilder header = new StringBuilder();
+		
+		Future<ICNDBResponse> future = executorService.submit(new Callable<ICNDBResponse>() {
+
+			public ICNDBResponse call() throws Exception {
+
+				return icndbEndpoint.random(header);
+			}
+		});
+		
+		ICNDBResponse icndbResponse = future.get();
+		
+		assertNotNull(icndbResponse);
 		
 		String server = header.toString();
 		assertTrue(server != null);
 		assertTrue(!server.equals(""));
+	}
+	
+	/**
+	 * <p>Tears down the test case by shutting down the 
+	 * {@link #executorService}.
+	 * 
+	 * @throws java.lang.Exception
+	 * 			if the tear-down failed
+	 */
+	@AfterClass
+	public static void tearDown() throws Exception {
+		
+		executorService.shutdownNow();
 	}
 }
